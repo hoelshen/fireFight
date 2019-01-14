3e1d6f16dcf3198162fd5c0058d9b8ea46b20872
unclexiao <449129722@qq.com>
2019-01-14 18:53:44 +0800

    处理冲突，合并版本

--- src/pages/auth/auth.vue
+++ src/pages/auth/auth.vue
@@ -1,21 +1,9 @@
 <template>
-<<<<<<< HEAD
     <view class="app">
-            <div class="photo-circle circle">
-                <div class="circle" @click="takePhoto">
-                    <image v-if="imageUrl!=null"  class="userinfo-avatar" :src="imageUrl" background-size="cover"/>
-                    <image v-else class="userinfo-avatar"   :src="userInfo.avatarUrl" alt="选择头像" background-size="cover"/>
-                </div>
-            </div>
-            <div class="block">
-                <input type="text" placeholder="设置笔名">
-=======
-<view class="app">
         <div class="photo-circle circle">
             <div class="circle" @click="takePhoto">
-                <image v-if="imgUrl!=null" mode="aspectFit" class="userinfo-avatar" src="imgUrl" background-size="cover"/>
-                <image v-else class="userinfo-avatar" mode="aspectFit" :src="userInfo.avatarUrl" alt="选择头像" background-size="cover"/>
->>>>>>> 1b9ac7a92960228246b8760a77d161f38103f0c2
+                <image v-if="imgUrl!=null" mode="aspectFit" class="userinfo-avatar" src="imgUrl" background-size="cover" />
+                <image v-else class="userinfo-avatar" mode="aspectFit" :src="userInfo.avatarUrl" alt="选择头像" background-size="cover" />
             </div>
 
             <span>你可以随时点击头像和笔名来修改它们</span>
@@ -23,58 +11,44 @@
             <div>
                 <button @click="save">保存</button>
             </div>
+        </div>
     </view>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            imageUrl:'https://user-images.githubusercontent.com/20720117/48262986-80e02780-e45f-11e8-8426-2872916adad9.png',
-            userInfo:{
-                avatarUrl:""
-            }
-        }
+  data() {
+    return {
+      imageUrl:
+        "https://user-images.githubusercontent.com/20720117/48262986-80e02780-e45f-11e8-8426-2872916adad9.png",
+      userInfo: {
+        avatarUrl: ""
+      }
+    };
+  },
+  methods: {
+    takePhoto() {
+      wx.chooseImage({
+        count: 1,
+        sizeType: ["original", "compressed"],
+        sourceType: ["album", "camera"],
+        success: function(res) {
+          // tempFilePath可以作为img标签的src属性显示图片
+          const tempFilePaths = res.tempFilePaths;
+          console.log("tempFilePaths", tempFilePaths[0]);
+          this.$request.uploadFile(tempFilePaths[0]).then(res => {
+            console.log(res);
+          });
+        }.bind(this)
+      });
     },
-    methods: {
-        takePhoto() {
-            wx.chooseImage({
-<<<<<<< HEAD
-                count: 1,
-                sizeType: ['original', 'compressed'],
-                sourceType: ['album', 'camera'],
-                success(res) {
-                    // tempFilePath可以作为img标签的src属性显示图片
-                    const tempFilePaths = res.tempFilePaths
-                    console.log('tempFilePaths', tempFilePaths[0])
-                },
-                fail(e){
-                    console.log(e)
-                }
-            })
-=======
-            count: 1,
-            sizeType: ['original', 'compressed'],
-            sourceType: ['album', 'camera'],
-            success:function(res) {
-                // tempFilePath可以作为img标签的src属性显示图片
-                const tempFilePaths = res.tempFilePaths
-                console.log('tempFilePaths', tempFilePaths[0])
-                this.$request.uploadFile(tempFilePaths[0]).then((res)=>{
-                    console.log(res)
-                });
-              }.bind(this)
-            }) 
->>>>>>> 1b9ac7a92960228246b8760a77d161f38103f0c2
-        },
-        save(){
-            this.$router.push({query: '',path:'/pages/myInfo/index'})
-        }      
-    },
-
-}
+    save() {
+      this.$router.push({ query: "", path: "/pages/myInfo/index" });
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
-.app{
+.app {
   padding-top: 20px;
 }
 </style>
