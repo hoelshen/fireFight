27f83e3be3d8a38e972468f04e082fea6e94d28f
huangpeng <huangpeng@meizu.com>
2020-04-27 22:10:35 +0800

    添加安装维保图片上传功能 #0

--- src/pages/installpage/installpage.vue
+++ src/pages/installpage/installpage.vue
@@ -54,9 +54,38 @@
         class="input"
         type="textarea"
         maxlength="200"
-        placeholder="输入200字以内的备注信息"
+        placeholder="请输入设备地址，以及IMEI号"
         @input="bindRemark"
       />
+    </div>
+
+    <div class="photos flex">
+      <div
+        v-for="(item, index) in fimage"
+        :key="index"
+        style="height: 120rpx; width: 120rpx; margin-right: 30rpx;"
+      >
+        <image
+          class="photo"
+          :src="item"
+        />
+        <img
+          class="leftTop"
+          src="/static/png/error.png"
+          @click="clearImg(index)"
+        >
+      </div>
+      <!-- v-if="form.fimage.length != 3" -->
+      <div
+        v-if="fimage.length != 3"
+        class="photoDiv"
+      >
+        <image
+          class="addPhoto"
+          src="/static/png/error1.png"
+          @click="upFile"
+        />
+      </div>
     </div>
 
     <div
@@ -85,11 +114,14 @@ export default {
           remark: '',
           type: '请选择',
           adviceItem: '请选择',
-          installId: 0
+          installId: 0,
+          fimage: [],
+          fOperatorUser: ''
       }
   },
   onLoad(opt) {
     this.params = wx.getStorageSync('userAddress')
+    this.fOperatorUser = wx.getStorageSync('userName');
   },
   onShow() {
   },
@@ -111,12 +143,43 @@ export default {
     bindRemark(e) {
         this.remark = e.detail.value;
     },
+    upFile(){
+      let sourceType = ['album', 'camera'];
+      const that = this;
+      wx.chooseImage({
+        count: 1,
+        sizeType: ["compressed"],
+        sourceType: sourceType,
+        success: function(res) {
+          wx.showLoading({
+            title: "上传中",
+            mask: true
+          });
+          const tempFilePaths = res.tempFilePaths;
+          that.$request.uploadFile(tempFilePaths[0]).then(
+            function(res) {
+              let data = res.data;
+              that.fimage.push(data);
+              wx.hideLoading();
+            }.bind(that)
+          );
+        }.bind(this),
+        fail(e) {
+          wx.hideLoading();
+        }
+      });
+    },
+    clearImg(i){
+      this.fimage.splice(i, 1)
+    },
     confirmBtn() {
         let postData = {
             'installId': this.installId,
             'installTime': this.installTime,
             'type': this.type,
             'remark': this.remark,
+            'photoUrl': this.fimage.join(','),
+            'fOperatorUser': this.fOperatorUser,
             'isJson': true
         }
         this.$request
@@ -206,6 +269,39 @@ export default {
     background:#fff;
     margin: 300rpx auto 0;
 }
-  
+  .photos{
+    margin:60rpx 40rpx;
+    position: relative;
+    height: 120rpx;
+    .photo{
+      width: 120rpx;
+      height: 120rpx;
+      margin-right: 30rpx; 
+      background-color: #eeeeee;
+      position: absolute;
+    }
+    .leftTop{
+      position: relative;
+      top: -5rpx;
+      right: -95rpx;
+      text-align:center;
+      width: 32rpx !important;
+      height: 32rpx !important;
+    }
+    .photoDiv{
+      width:120rpx;
+      height:120rpx;
+      text-align: center;
+      line-height: 130rpx;
+      // border-width: 1rpx;
+      // border-style:dashed;
+      // background-color: #ffffff;
+      .addPhoto{
+        width:120rpx;
+        height: 120rpx;
+      }
+    }
+
+  }
 }
 </style>
